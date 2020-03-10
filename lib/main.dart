14286346c59d29e25247ddfa8f87301d685ac4e6
shimeng wang <1399694975@qq.com>
2020-03-10 11:23:10 +0800

    success

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'package:connectivity/connectivity.dart';
+import 'dart:async';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  Future<bool> getWidget()  async{
+    var connectivityResult = await (Connectivity().checkConnectivity());
+    return connectivityResult != ConnectivityResult.none;
+  }
 
   @override
   Widget build(BuildContext context) {
-  return new MaterialApp(
-    routes: {
-      "/": (_) => new WebviewScaffold(
-        url: "http://www.taobao.com",
-        useWideViewPort: true,
-        displayZoomControls:true,
-        withOverviewMode: true,
-//        appBar: new AppBar(
-//          title: new Text(""),
-//        ),
-      ),
-    },
-  );
+    return MyHomePage();
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -42,68 +26,105 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  Future<bool> getWidget()  async{
+      var connectivityResult = await (Connectivity().checkConnectivity());
+      return connectivityResult != ConnectivityResult.none;
+  }
+
+  void initState() {
+    settimerout();
+  }
+  void settimerout(){
+//    const timeout = const Duration(seconds: 10);
+//    Timer(timeout, () {
+//      //到时回调
+//      setState(() {
+//      });
+//      print('afterTimer='+DateTime.now().toString());
+//    });
+
+    const period = const Duration(seconds: 10);
+    Timer.periodic(period, (timer) {
+      print("定时调用");
+      print('currentTime='+DateTime.now().toString());
+            setState(() {
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    print("build");
+    return FutureBuilder<bool>(
+          future:getWidget(),
+          builder: (BuildContext context, AsyncSnapshot snapshot){
+            if (snapshot.connectionState == ConnectionState.done) {
+              if (snapshot.hasError ) {
+                // 请求失败，显示错误
+                return new MaterialApp(
+                    home:
+                    AlertDialog(
+                      title: Text("提示"),
+                      content: Text("检查到目前网络不畅通，请检查网络设置。我们将于10s之后再次检测"),
+                      actions: <Widget>[
+                        FlatButton(
+                          child: Text("确定"),
+                          onPressed: () => Navigator.of(context).pop(), //关闭对话框
+                        ),
+                      ],
+                    )
+                );
+              } else{
+                // 请求成功，显示数据
+                print("result");
+                print(snapshot.data);
+                print(snapshot.connectionState);
+                print("bbb");
+                if(snapshot.data == true ){
+                  return new MaterialApp(
+                    routes: {
+                      "/": (_) => new WebviewScaffold(
+                        url: "http://www.necta.online/anfang/wall/shequ.html",
+                        useWideViewPort: true,
+                        displayZoomControls:true,
+                        withOverviewMode: true,
+                      ),
+                    },
+                  );
+                }else{
+//                  settimerout();
+                  return new MaterialApp(
+                      home: Builder(
+                        builder: (context) =>
+                            AlertDialog(
+                          title: Text("提示"),
+                          content: Text("检查到目前网络不畅通，请检查网络设置。我们将于10s之后再次检测"),
+                          actions: <Widget>[
+                            FlatButton(
+                              child: Text("确定"),
+                              onPressed: () => Navigator.of(context).pop(), //关闭对话框
+                            ),
+                          ],
+
+                        ),
+                      )
+                  );
+
+                }
+
+              }
+            }
+            else {
+              // 请求未结束，显示loading
+                return CircularProgressIndicator();
+            }
+
+          }
+      );
+
+
+//    );
   }
 }
+
